4163a0bc4ddf10b63f2dc6b07f77e8f43824ea8b
Mathew <brzoza.mathew@gmail.com>
2024-04-01 20:56:12 +0100

    'added-history-chart-with-sentiment-score'

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,3 +1,4 @@
+import HistoryChart from "@/components/HistoryChart";
 import { getUserByClerkId } from "@/utils/auth";
 import { prisma } from "@/utils/db";
 
@@ -7,11 +8,29 @@ const getData = async () => {
     where: {
       userId: user.id,
     },
+    orderBy: {
+      createdAt: "asc",
+    },
   });
+
+  const sum = analyses.reduce(
+    (all, current) => all + current.sentimentScore,
+    0
+  );
+  const avg = Math.round(sum / analyses.length);
+  return { analyses, avg };
 };
 
-const History = () => {
-  return <div>History</div>;
+const History = async () => {
+  const { avg, analyses } = await getData();
+  return (
+    <div className="w-full h-full">
+      <div>{`Avg. Sentiment ${avg}`}</div>
+      <div className="w-full h-full">
+        <HistoryChart data={analyses} />
+      </div>
+    </div>
+  );
 };
 
 export default History;
